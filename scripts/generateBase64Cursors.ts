c4890d96170799bce620ce4fe2cd50e1cf3f1983
alexharri <alexharri2919@gmail.com>
2022-01-30 12:43:22 +0000

    add cursor png images and a script to generate base64 cursors

---
+++ scripts/generateBase64Cursors.ts
@@ -0,0 +1,32 @@
+import fs from "fs";
+import path from "path";
+
+const srcDir = path.resolve(__dirname, "../src");
+const cursorsDir = path.resolve(srcDir, "./core/utils/cursor");
+const pngDir = path.resolve(cursorsDir, "./png");
+const outFile = path.resolve(cursorsDir, "./base64Cursors.ts");
+// const base64Dir = path.resolve(cursorsDir, "./base64");
+
+const cursorFileNames = fs.readdirSync(pngDir);
+
+const out: Record<string, string> = {};
+
+for (const fileName of cursorFileNames) {
+  const cursorPath = path.resolve(pngDir, `./${fileName}`);
+
+  const withoutDotPng = fileName.split(".png")[0];
+
+  // const base64Path = path.resolve(base64Dir, `./${fileName}`);
+  const content = fs.readFileSync(cursorPath, "base64");
+  out[withoutDotPng] = `url(data:image/png;base64,${content})`;
+}
+
+let str = `export const base64Cursors = {\n`;
+
+for (const [key, value] of Object.entries(out)) {
+  str += `  ${key}: "${value}",\n`;
+}
+
+str += "};\n";
+
+fs.writeFileSync(outFile, str, "utf-8");
