22b35a5c0ccc94b0db423e793ed6465b5dc15955
alexharri <alexharri2919@gmail.com>
2022-02-01 20:14:52 +0000

    animate zoom so that it appears more smooth

--- src/core/handlers/zoom.ts
+++ src/core/handlers/zoom.ts
@@ -1,9 +1,11 @@
 import { CANVAS_END_START_BUFFER, ZOOM_FAC } from "~/core/constants";
+import { getGraphEditorYBounds } from "~/core/render/yBounds";
 import { requestAction } from "~/core/state/requestAction";
 import { ActionOptions } from "~/core/state/stateTypes";
-import { capToRange } from "~/core/utils/math/math";
+import { animate } from "~/core/utils/animate";
+import { capToRange, lerp } from "~/core/utils/math/math";
 import { Vec2 } from "~/core/utils/math/Vec2";
-import { SomeMouseEvent } from "~/types/commonTypes";
+import { SomeMouseEvent, ViewBounds, YBounds } from "~/types/commonTypes";
 
 interface Options {
   e: SomeMouseEvent;
@@ -14,35 +16,59 @@ export function onZoom(actionOptions: ActionOptions, options: Options) {
   const { e, type } = options;
 
   requestAction({ userActionOptions: actionOptions }, (params) => {
-    const { view } = params;
-    const { viewport, viewBounds, allowExceedViewBounds } = view.state;
+    const { primary, view, ephemeral } = params;
+    const { viewport, viewBounds, allowExceedViewBounds, length } = view.state;
 
     const mousePos = Vec2.fromEvent(e)
       .subX(viewport.left)
       .subX(CANVAS_END_START_BUFFER);
     const t = mousePos.x / (viewport.width - CANVAS_END_START_BUFFER * 2);
 
-    let newBounds: [number, number];
+    let nextViewBounds: [number, number];
 
     const LOW = allowExceedViewBounds ? -Infinity : 0;
     const HIGH = allowExceedViewBounds ? Infinity : 0;
 
     if (type === "zoom_out") {
       const add = Math.abs(viewBounds[0] - viewBounds[1]) * ZOOM_FAC * 2;
-      newBounds = [
+      nextViewBounds = [
         capToRange(LOW, HIGH, viewBounds[0] - add * t),
         capToRange(LOW, HIGH, viewBounds[1] + add * (1 - t)),
       ];
     } else {
       const remove = Math.abs(viewBounds[0] - viewBounds[1]) * ZOOM_FAC;
-      newBounds = [
+      nextViewBounds = [
         viewBounds[0] + remove * t,
         viewBounds[1] - remove * (1 - t),
       ];
     }
 
-    view.dispatch((actions) => actions.setFields({ viewBounds: newBounds }));
+    const yBounds = getGraphEditorYBounds({
+      length,
+      timelines: primary.state.timelines,
+      viewBounds,
+    });
+    const nextYBounds = getGraphEditorYBounds({
+      length,
+      timelines: primary.state.timelines,
+      viewBounds: nextViewBounds,
+    });
 
-    params.submit({ name: "Zoom" });
+    animate({ duration: 150 }, (t) => {
+      const currYBounds: YBounds = [
+        lerp(yBounds[0], nextYBounds[0], t),
+        lerp(yBounds[1], nextYBounds[1], t),
+      ];
+      const currViewBounds: ViewBounds = [
+        lerp(viewBounds[0], nextViewBounds[0], t),
+        lerp(viewBounds[1], nextViewBounds[1], t),
+      ];
+      ephemeral.dispatch((actions) =>
+        actions.setFields({ yBounds: currYBounds })
+      );
+      view.dispatch((actions) =>
+        actions.setFields({ viewBounds: currViewBounds })
+      );
+    }).then(() => params.submit({ name: "Zoom" }));
   });
 }
---
+++ src/core/utils/animate.ts
@@ -0,0 +1,76 @@
+import BezierEasing from "bezier-easing";
+import { lerp } from "~/core/utils/math/math";
+
+export interface AnimateOptions {
+  from?: number;
+  to?: number | (() => number);
+  duration?: number;
+  bezier?: [number, number, number, number];
+  callback?: any;
+}
+
+interface AnimationOpts {
+  from: number;
+  to: number | (() => number);
+  duration: number;
+  bezier: [number, number, number, number];
+}
+
+const defaultOpts: AnimationOpts = {
+  from: 0,
+  to: 1,
+  duration: 500,
+  bezier: [0.46, 0.19, 0.13, 0.98],
+};
+
+export function animate(
+  options: AnimateOptions,
+  fn: (t: number) => void
+): Promise<boolean> & { cancel: () => void } {
+  let cancelled = false;
+
+  const promise = new Promise<boolean>((resolve) => {
+    const opts = {
+      ...defaultOpts,
+      ...options,
+    };
+
+    const [p0, p1, p2, p3] = opts.bezier;
+    const easing = BezierEasing(p0, p1, p2, p3);
+    const startTime = Date.now();
+
+    let f = opts.from;
+
+    const tick = () => {
+      if (cancelled) {
+        resolve(true);
+        return;
+      }
+
+      const actualF = (Date.now() - startTime) / opts.duration;
+      f = easing(actualF);
+
+      if (actualF >= 1) {
+        f = 1;
+      } else {
+        requestAnimationFrame(tick);
+      }
+
+      const to = typeof opts.to === "number" ? opts.to : opts.to();
+
+      fn(lerp(opts.from, to, f));
+
+      if (f === 1) {
+        resolve(false);
+      }
+    };
+
+    tick();
+  }) as Promise<boolean> & { cancel: () => void };
+
+  promise.cancel = () => {
+    cancelled = true;
+  };
+
+  return promise;
+}
