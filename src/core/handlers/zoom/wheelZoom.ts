a5b57cb7f2cbb91ac60977dc0c2253ac3bb020ca
alexharri <alexharri2919@gmail.com>
2022-02-06 11:46:57 +0000

    allow the wheel event to exceed the view bounds when appropriate

--- src/core/handlers/zoom/wheelZoom.ts
+++ src/core/handlers/zoom/wheelZoom.ts
@@ -14,7 +14,7 @@ export function onWheelZoom(actionOptions: ActionOptions, options: Options) {
 
   const fac = lerp(1, -deltaY < 0 ? 1.15 : 0.85, capToRange(0, 2, impact));
 
-  const { viewBounds, viewport } = actionOptions.initialState.view;
+  const { viewBounds, viewport, allowExceedViewBounds } = actionOptions.initialState.view;
 
   const mousePos = Vec2.fromEvent(e).subX(viewport.left);
   const t = mousePos.x / viewport.width;
@@ -27,14 +27,16 @@ export function onWheelZoom(actionOptions: ActionOptions, options: Options) {
   const remove = Math.abs(viewBounds[0] - viewBounds[1]) * (1 - fac);
   let newBounds: [number, number] = [viewBounds[0] + remove * t, viewBounds[1] - remove * (1 - t)];
 
-  if (newBounds[0] < 0 && newBounds[1] > 1) {
-    newBounds = [0, 1];
-  } else if (newBounds[0] < 0) {
-    newBounds[1] = Math.min(1, newBounds[1] + Math.abs(newBounds[0]));
-    newBounds[0] = 0;
-  } else if (newBounds[1] > 1) {
-    newBounds[0] = Math.max(0, newBounds[0] - (newBounds[1] - 1));
-    newBounds[1] = 1;
+  if (!allowExceedViewBounds) {
+    if (newBounds[0] < 0 && newBounds[1] > 1) {
+      newBounds = [0, 1];
+    } else if (newBounds[0] < 0) {
+      newBounds[1] = Math.min(1, newBounds[1] + Math.abs(newBounds[0]));
+      newBounds[0] = 0;
+    } else if (newBounds[1] > 1) {
+      newBounds[0] = Math.max(0, newBounds[0] - (newBounds[1] - 1));
+      newBounds[1] = 1;
+    }
   }
 
   requestAction({ userActionOptions: actionOptions }, (params) => {
