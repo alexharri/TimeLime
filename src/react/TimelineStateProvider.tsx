48b0db31f690f5e4c887e10d230b6fc2eb3ddd6d
alexharri <alexharri2919@gmail.com>
2022-02-08 19:58:41 +0000

    aggressively refactor TimelineStateProvider

--- src/react/TimelineStateProvider.tsx
+++ src/react/TimelineStateProvider.tsx
@@ -1,14 +1,7 @@
-import React, { useCallback, useMemo, useRef } from "react";
+import React, { useMemo, useRef } from "react";
 import { RenderState } from "~/core/state/stateTypes";
-import { applyControlPointShift } from "~/core/timeline/applyControlPointShift";
-import { applyNewControlPointShift } from "~/core/timeline/applyNewControlPointShift";
-import { applyTimelineKeyframeShift } from "~/core/timeline/applyTimelineKeyframeShift";
-import { getTimelineValueAtIndex } from "~/core/timeline/timelineValueAtIndex";
-import {
-  ITimelineStateContext,
-  TimelineStateContext,
-  UseTimelineResult,
-} from "~/react/TimelineStateContext";
+import { getUseTimelineResult } from "~/react/getUseTimelineResult";
+import { ITimelineStateContext, TimelineStateContext } from "~/react/TimelineStateContext";
 import { UseTimelineStateListener } from "~/react/types";
 import { useMonitorRenderState } from "~/react/useMonitorRenderState";
 
@@ -20,56 +13,21 @@ interface Props {
 }
 
 export const TimelineStateProvider: React.FC<Props> = (props) => {
+  const { renderStateRef } = props;
+
   const idRef = useRef(0);
   const listeners = useMemo<UseTimelineStateListener[]>(() => [], []);
 
-  const currStateRef = props.renderStateRef;
-
-  const getUseTimelineResult = useCallback((timelineId: string): UseTimelineResult => {
-    const { primary, selection, view, ephemeral } = currStateRef.current;
-
-    const { frameIndex } = view;
-    const { keyframeShift, controlPointShift, newControlPointShift } = ephemeral;
-
-    let timeline = primary.timelines[timelineId];
-    const timelineSelection = selection[timelineId];
-
-    if (keyframeShift) {
-      timeline = applyTimelineKeyframeShift({ timeline, timelineSelection, keyframeShift });
-    }
-
-    if (controlPointShift) {
-      timeline = applyControlPointShift({ timeline, timelineSelection, controlPointShift });
-    }
-
-    if (newControlPointShift) {
-      timeline =
-        timeline.id === newControlPointShift.timelineId
-          ? applyNewControlPointShift(timeline, newControlPointShift)
-          : timeline;
-    }
-
-    const value = getTimelineValueAtIndex({ frameIndex, timeline });
-
-    return { timeline, selection: timelineSelection, value };
-  }, []);
-
   useMonitorRenderState({
     listeners,
-    getCurrentState: () => currStateRef.current,
-    executeCallback: (listener) => executeListener(listener),
+    getCurrentState: () => renderStateRef.current,
+    executeCallback: ({ callback, timelineId }) =>
+      callback(getUseTimelineResult(timelineId, renderStateRef.current)),
   });
 
-  const executeListener = useCallback(
-    ({ callback: listener, timelineId }: UseTimelineStateListener) => {
-      listener(getUseTimelineResult(timelineId));
-    },
-    [],
-  );
-
-  const value = useMemo<ITimelineStateContext>(() => {
+  const contextValue = useMemo<ITimelineStateContext>(() => {
     return {
-      getTimelineValue: getUseTimelineResult,
+      getTimelineValue: (timelineId) => getUseTimelineResult(timelineId, renderStateRef.current),
       subscribeToTimeline: (timelineId, callback) => {
         const id = ++idRef.current;
         listeners.push({ id, timelineId, callback });
@@ -88,6 +46,8 @@ export const TimelineStateProvider: React.FC<Props> = (props) => {
   }, []);
 
   return (
-    <TimelineStateContext.Provider value={value}>{props.children}</TimelineStateContext.Provider>
+    <TimelineStateContext.Provider value={contextValue}>
+      {props.children}
+    </TimelineStateContext.Provider>
   );
 };
