d893ad66f3e0655d75b8a90e6cc6b294f84e8f41
alexharri <alexharri2919@gmail.com>
2022-02-12 13:30:36 +0000

    add multiple timelines to CSS Keyframes example

--- src/css-keyframes/CSSKeyframe.tsx
+++ src/css-keyframes/CSSKeyframe.tsx
@@ -7,19 +7,43 @@ import { Timeline } from "~/css-keyframes/Timeline";
 
 const translateXKeyframes = curvesToKeyframes([
   [
-    [0, 0],
-    [50, 50],
+    [0, 75],
+    [0, 37.6],
+    [18.1, 30],
+    [35, 30],
   ],
   [
-    [50, 50],
-    [100, 25],
+    [35, 30],
+    [38.8, 62.6],
+    [67.1, 61.8],
+    [86, 61.3],
   ],
 ]);
+
+const translateYKeyframes = curvesToKeyframes([
+  [
+    [0, 0],
+    [24.3, 0],
+    [16.9, 18.7],
+    [29, 22.5],
+  ],
+  [
+    [29, 22.5],
+    [47.3, 28.3],
+    [64.5, 14.1],
+    [91, 14.1],
+  ],
+]);
+
 const initialState: PrimaryState = {
   timelines: {
     [PropertyIds.TranslateX]: {
       id: PropertyIds.TranslateX,
       keyframes: translateXKeyframes,
+    },
+    [PropertyIds.TranslateY]: {
+      id: PropertyIds.TranslateY,
+      keyframes: translateYKeyframes,
     },
   },
 };
