bc5a2740cbc071ac79119111c5a926351de295e5
alexharri <alexharri2919@gmail.com>
2022-02-12 20:01:32 +0000

    remove unused types

--- src/types/reducerTypes.ts
+++
@@ -1,6 +0,0 @@
-import { timelineActions } from "~/core/state/timeline/timelineActions";
-import { timelineSelectionActions } from "~/core/state/timelineSelection/timelineSelectionActions";
-import { ActionsReturnType } from "~/types/commonTypes";
-
-export type TimelineAction = ActionsReturnType<typeof timelineActions>;
-export type TimelineSelectionAction = ActionsReturnType<typeof timelineSelectionActions>;
