fa6ef34416e956f7fd32849cc71ec923c750ee41
alexharri <alexharri2919@gmail.com>
2022-02-13 13:24:23 +0000

    fix CSS Keyframes example not running

--- examples/css-keyframes/next.config.js
+++ examples/css-keyframes/next.config.js
@@ -4,11 +4,11 @@
 const path = require("path");
 const dev = process.env.NODE_ENV !== "production";
 
-const withTM = require("next-transpile-modules")([
-  path.resolve(__dirname, "../core"),
-  path.resolve(__dirname, "../types"),
-  path.resolve(__dirname, "../react"),
-]);
+const libs = ["core", "types", "react", "undo-redo"];
+
+const withTM = require("next-transpile-modules")(
+  libs.map((lib) => path.resolve(__dirname, `../../libs/${lib}`)),
+);
 
 /** @type {import("next/dist/server/config").NextConfig} */
 const nextConfig = {
@@ -19,7 +19,11 @@ const nextConfig = {
     ignoreDuringBuilds: true,
   },
   webpack: (config) => {
-    config.resolve.alias["~"] = path.resolve(__dirname, "../");
+    for (const lib of libs) {
+      const libSrc = `../../libs/${lib}/src`;
+      config.resolve.alias[`timelime/${lib}`] = path.resolve(__dirname, `${libSrc}/index.ts`);
+      config.resolve.alias[`~${lib}`] = path.resolve(__dirname, libSrc);
+    }
     return config;
   },
 
