7655cb7d744d52e1b66d2167b6fe98fdc54859c9
alexharri <alexharri2919@gmail.com>
2022-02-13 10:26:28 +0000

    apply the value after splitting the keyframes

--- src/react/hook/useTimelineDragValue.ts
+++ src/react/hook/useTimelineDragValue.ts
@@ -31,66 +31,65 @@ export function useTimelineDragValue(options: Options) {
         const { frameIndex } = view.state;
 
         onValueChangeFn.current = (value) => {
-          const update = (value: number) => {
-            const { timelines } = primary.state;
-            const timeline = timelines[timelineId];
+          const { timelines } = primary.state;
+          const timeline = timelines[timelineId];
 
-            let keyframe: TimelineKeyframe | null = null;
+          let keyframe: TimelineKeyframe | null = null;
 
-            for (let i = 0; i < timeline.keyframes.length; i += 1) {
-              if (timeline.keyframes[i].index === frameIndex) {
-                keyframe = timeline.keyframes[i];
-              }
+          for (let i = 0; i < timeline.keyframes.length; i += 1) {
+            if (timeline.keyframes[i].index === frameIndex) {
+              keyframe = timeline.keyframes[i];
+            }
+          }
+
+          if (keyframe) {
+            const nextKeyframe = { ...keyframe, value };
+            primary.dispatch((actions) => actions.setKeyframe(timelineId, nextKeyframe));
+            return;
+          }
+
+          const keyframes = timeline.keyframes;
+
+          if (frameIndex < keyframes[0].index) {
+            const k = createTimelineKeyframe(timeline, value, frameIndex);
+            primary.dispatch((actions) => actions.setKeyframe(timelineId, k));
+            return;
+          }
+
+          if (frameIndex > keyframes[keyframes.length - 1].index) {
+            const k = createTimelineKeyframe(timeline, value, frameIndex);
+            primary.dispatch((actions) => actions.setKeyframe(timelineId, k));
+            return;
+          }
+
+          for (let i = 0; i < keyframes.length; i += 1) {
+            if (keyframes[i].index > frameIndex) {
+              continue;
             }
 
-            if (keyframe) {
-              const nextKeyframe = { ...keyframe, value };
-              primary.dispatch((actions) => actions.setKeyframe(timelineId, nextKeyframe));
-              return;
+            if (keyframes[i].index === frameIndex) {
+              return keyframes[i].value;
             }
 
-            const keyframes = timeline.keyframes;
-
-            if (frameIndex < keyframes[0].index) {
-              const k = createTimelineKeyframe(timeline, value, frameIndex);
-              primary.dispatch((actions) => actions.setKeyframe(timelineId, k));
-              return;
+            if (frameIndex > keyframes[i + 1].index) {
+              continue;
             }
 
-            if (frameIndex > keyframes[keyframes.length - 1].index) {
-              const k = createTimelineKeyframe(timeline, value, frameIndex);
-              primary.dispatch((actions) => actions.setKeyframe(timelineId, k));
-              return;
+            const [k0, k, k1] = splitKeyframesAtIndex(
+              timeline,
+              keyframes[i],
+              keyframes[i + 1],
+              frameIndex,
+            );
+
+            k.value = value;
+
+            for (const keyframe of [k0, k, k1]) {
+              primary.dispatch((actions) => actions.setKeyframe(timelineId, keyframe));
             }
-
-            for (let i = 0; i < keyframes.length; i += 1) {
-              if (keyframes[i].index > frameIndex) {
-                continue;
-              }
-
-              if (keyframes[i].index === frameIndex) {
-                return keyframes[i].value;
-              }
-
-              if (frameIndex > keyframes[i + 1].index) {
-                continue;
-              }
-
-              const [k0, k, k1] = splitKeyframesAtIndex(
-                timeline,
-                keyframes[i],
-                keyframes[i + 1],
-                frameIndex,
-              );
-
-              for (const keyframe of [k0, k, k1]) {
-                primary.dispatch((actions) => actions.setKeyframe(timelineId, keyframe));
-              }
-            }
-          };
-
-          update(value);
+          }
         };
+
         onValueChangeFn.current(value);
       }),
     );
